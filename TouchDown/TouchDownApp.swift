f14ea01b361668ca3875b7e5cec5bc4de8a42926
Geraud Luku <geraud.luku@dme.systems>
2023-03-01 11:08:37 +0100

    Initial Commit

---
+++ TouchDown/TouchDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDownApp.swift
+//  TouchDown
+//
+//  Created by LVMM on 01/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct TouchDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
